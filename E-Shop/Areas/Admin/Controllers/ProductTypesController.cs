0463d97028920e088182d75708190aa5428ec798
mahfuz khan <mahfuzpust@gmail.com>
2023-06-07 00:21:56 +0600

    Create-ProductType/Model

--- E-Shop/Areas/Admin/Controllers/ProductTypesController.cs
+++ E-Shop/Areas/Admin/Controllers/ProductTypesController.cs
@@ -1,4 +1,5 @@
 ﻿using E_Shop.Data;
+using E_Shop.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Shop.Areas.Admin.Controllers
@@ -16,6 +17,29 @@ namespace E_Shop.Areas.Admin.Controllers
         {
             var data = dBContext.ProductTypes.ToList();
             return View(data);
+        }
+
+        //Get Method Product Types Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post Method Product Types
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes model)
+        {
+            if (ModelState.IsValid)
+            {
+                dBContext.ProductTypes.Add(model);
+                await dBContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(model);
+            }
         }
     }
 }
