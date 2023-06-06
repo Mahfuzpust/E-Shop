a3cecfaaa0c4dd42681c19df22fc7213a46969b3
mahfuz khan <mahfuzpust@gmail.com>
2023-06-06 23:53:53 +0600

    ProductTypes

---
+++ E-Shop/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,21 @@
+﻿using E_Shop.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Shop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private readonly ApplicationDBContext dBContext;
+        public ProductTypesController(ApplicationDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
+        public IActionResult Index()
+        {
+            var data = dBContext.ProductTypes.ToList();
+            return View(data);
+        }
+    }
+}
